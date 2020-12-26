c209989985dbc3ad17769605000646de2a3f6253
Mike Mwanje <mwanjemike767@gmail.com>
2020-12-26 13:09:23 +0300

    Using fragments

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import App from "./App"
+import App from "./App";
+
+function AppTwo() {
+  return <h1>This is the second app</h1>;
+}
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <React.Fragment>
+    <App />
+    <AppTwo />
+  </React.Fragment>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
