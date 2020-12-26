daccce93598170e088ac75ef182e671184972430
Mike Mwanje <mwanjemike767@gmail.com>
2020-12-26 14:48:04 +0300

    Working with useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect } from "react";
 
 function App() {
     const [emotion, setEmotion] = useState('happy');
+    const [secondary, setSecondary ] = useState('tired');
+    
+    useEffect(() => {
+        console.log(`It's ${emotion} around here`)
+    }, [emotion]);
+    useEffect(() => {
+        console.log(`It's ${secondary} around here`)
+    }, [secondary]);
+
     return (
         <>
         <h1>Hello</h1>
-        <p>Current emotion is {emotion}</p>
+        <p>Current emotion is {emotion} and {secondary}</p>
         <button onClick={() => setEmotion('happy')}>
-            Happy
+        Make Happy
+        </button>
+        <button onClick={() => setSecondary('crabby')}>
+            Make Crabby
         </button>
         <button onClick={() => setEmotion('frustrated')}>
             Frustrate
