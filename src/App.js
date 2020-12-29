fdd4a279433bb5efef8c44746d5179562e579b68
Mike Mwanje <mwanjemike767@gmail.com>
2020-12-29 17:25:46 +0300

    Displaying data from an API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 // https://api.github.com/users/123MwanjeMike
 
 function App({ login }) {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${login}`)
-          .then((response) => response.json())
-          .then(setData)
-    }, []);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
 
-    if(data){
-        return <div>{JSON.stringify(data)}</div>
-    }
+  if (data) {
     return (
-        <>User not available</>
+      <div>
+        <h1>{data.name}</h1>
+        <p>Location: {data.location}</p>
+        <p>Company: {data.company}</p>
+        <img src={data.avatar_url} alt={data.login} height='200' />
+      </div>
     );
+  }
+  return <>User not available</>;
 }
 
 export default App;
