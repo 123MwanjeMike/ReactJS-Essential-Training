623e13bc9618e093817ef7ee7956a24af5b11be7
Mike Mwanje <mwanjemike767@gmail.com>
2020-12-29 18:10:48 +0300

    Introduction to React testing library

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import { useState, useEffect } from "react";
 import "./App.css";
 
-// https://api.github.com/users/123MwanjeMike
-
-function App({ login }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [login]);
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (!data) return null;
+function App() {
 
   return (
     <div>
-      <h1>{data.name}</h1>
-      <p>Location: {data.location}</p>
-      <p>Company: {data.company}</p>
-      <img src={data.avatar_url} alt={data.login} height="200" />
+      <h1>Hello React Testing Library</h1>
     </div>
   );
 }
